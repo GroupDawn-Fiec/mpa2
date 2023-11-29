4f1222a8316f4ef75240802f4d49f21426ba6a9f
AngelT-lancervs <aatomala@espol.edu.ec>
2023-11-29 10:54:13 -0500

    new component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { FooterComponent } from './shared/footer/footer.component';
 import { NavComponent } from './shared/nav/nav.component';
 import { IndexComponent } from './pages/index/index.component';
 import { AboutComponent } from './pages/about/about.component';
+import { PreloaderComponent } from './shared/preloader/preloader.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FooterComponent, NavComponent, IndexComponent,AboutComponent],
+  imports: [CommonModule, RouterOutlet, FooterComponent, NavComponent, IndexComponent,AboutComponent, PreloaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
